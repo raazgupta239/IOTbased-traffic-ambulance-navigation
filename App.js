b04b7e7fd6ec7223423437da42bb8c0ba7e7283e
raaz <raaz@localhost.localdomain>
2025-01-12 23:30:47 +0545

    bug updatee

--- App.js
+++ App.js
@@ -87,6 +87,7 @@ export default function App() {
             component={VerifyOtp}
             options={{ headerShown: true, title: "Verify OTP" }}
           />
+          
           </>
           )}
         </Stack.Navigator>
